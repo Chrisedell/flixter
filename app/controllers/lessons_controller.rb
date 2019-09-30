661917e1f004008e2955e826a6ba7b819cb6f325
chrisedell <chrise.delladonna@gmail.com>
2019-09-30 19:40:01 +0000

    28 and 29 Permissions and Payments added

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,22 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson.section.course, only: [:show]
+  #  if current_user
+
   def show
   end
 
+ #unless current_user.enrolled_in?(current_lesson.section.course) 
+
+  
   private
+
+  def require_authorized_for_current_course
+    unless current_lesson.section.course.user.enrolled_in?(current_lesson) != current_user
+    redirect_to course_path, alert: 'You must be enrolled to view this lesson'
+    end
+    #render plain: "Unauthorized", status: :unauthorized
+  end
 
   helper_method :current_lesson
   def current_lesson
