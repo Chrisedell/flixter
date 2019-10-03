64383accfe3c835f54a776f1218df2b6ec81c614
chrisedell <chrise.delladonna@gmail.com>
2019-10-03 00:04:11 +0000

    39 Added static page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,20 @@ class StaticPagesController < ApplicationController
 
   def index
   end
+
+  def privacy
+  end
+
+  def careers
+  end
+
+  def about
+  end
+
+  def team
+  end
+
+  def contact
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'careers', to: 'static_pages#careers'
+  get 'about', to: 'static_pages#about'
+  get 'team', to: 'static_pages#team'
+  get 'contact', to: 'static_pages#contact'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
